a30c845fe68caefcd47e4349e914ee552c3fcc4b
JulianHwang <julianhwang@163.com>
2023-03-29 19:30:52 +0800

    day19-标题栏的设置

--- app/src/main/java/com/julian/myfirstapp/TestActivity.java
+++ app/src/main/java/com/julian/myfirstapp/TestActivity.java
@@ -3,6 +3,10 @@ package com.julian.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.blankj.utilcode.util.BarUtils;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        BarUtils.transparentStatusBar(this);
+        View statusBarView = findViewById(R.id.statusBarView);
+        statusBarView.getLayoutParams().height = BarUtils.getStatusBarHeight();
+        findViewById(R.id.iv_back).setOnClickListener(v -> {finish();});
+        ((TextView)findViewById(R.id.tv_title)).setText("村红特产");
     }
 }
