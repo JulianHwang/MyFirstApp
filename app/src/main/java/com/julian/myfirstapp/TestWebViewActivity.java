f39f83f71ba2e3bb2f6182d13db131f7bcb39cc0
JulianHwang <julianhwang@163.com>
2023-04-10 22:59:35 +0800

    day25-webview的常用设置和js交互

--- app/src/main/java/com/julian/myfirstapp/TestWebViewActivity.java
+++ app/src/main/java/com/julian/myfirstapp/TestWebViewActivity.java
@@ -3,18 +3,31 @@ package com.julian.myfirstapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.blankj.utilcode.util.ToastUtils;
 
 public class TestWebViewActivity extends AppCompatActivity {
 
     private WebView webView;
+    private ProgressBar progressBar;
+    private TextView tv_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +37,79 @@ public class TestWebViewActivity extends AppCompatActivity {
     }
 
     private void initView() {
-         webView = findViewById(R.id.webView);
-        WebSettings settings = webView.getSettings();
-        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);//禁止缓存
+        tv_title = (TextView)findViewById(R.id.tv_title);
+        findViewById(R.id.iv_back).setOnClickListener(v -> {onBackPressed();});
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);//禁止缓存
+        webSettings.setUseWideViewPort(true);//页面自适应
+        webSettings.setAllowFileAccess(true); // 允许访问文件
+        webSettings.setBuiltInZoomControls(true); // 设置显示缩放按钮
+        webSettings.setSupportZoom(false); // 是否支持缩放
+        webSettings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new JsOperation(),"android");
+        //允许页面有http和https混合资源的访问
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
 
         WebViewClient webViewClient = new WebViewClient() {
             @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url.contains("baidu")){
+                /*if (url.contains("baidu")){
                     ToastUtils.showShort("url包含baidu");
                     webView.loadUrl("http://www.hao123.com");
                     return true;
-                }
+                }*/
                 return false;
             }
 
-            @Nullable
             @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
-                return super.shouldInterceptRequest(view, url);
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+
+
+        };
+
+        WebChromeClient webChromeClient = new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                tv_title.setText(TextUtils.isEmpty(title)?"":title);
+
             }
         };
 
         webView.setWebViewClient(webViewClient);
-
-         webView.loadUrl("http://www.baidu.com");
+        webView.setWebChromeClient(webChromeClient);
+        //webView.loadUrl("http://www.baidu.com");
+        webView.loadUrl("http://192.168.1.107/testAndroid/test1.html");
     }
 
     @Override
@@ -57,6 +118,19 @@ public class TestWebViewActivity extends AppCompatActivity {
             webView.goBack();
         }else {
             finish();
+        }
+    }
+
+
+    public static class JsOperation{
+
+        public JsOperation(){
+
+        }
+
+        @JavascriptInterface
+        public void showLocalToast(String msgFromWeb){
+            ToastUtils.showShort(msgFromWeb);
         }
     }
 }
